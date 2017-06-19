a47d8f6de8d34b8450e2b221de34bf6bcded26d6
user <user@userde-MacBook-2.local>
2017-06-19 16:01:17 +0800

    v1

--- Myapp01/ViewController.swift
+++ Myapp01/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var labelMesg: UILabel!
+    @IBOutlet weak var btnClick: UIButton!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    @IBOutlet weak var label6: UILabel!
+    @IBAction func sayYa(_ sender: Any) {
+        labelMesg.text = createLottery()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +32,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    private func createLottery() -> String{
+        var setLottery:Set<Int> = []
+        var temp:Int
+        while setLottery.count < 6 {
+            temp = Int(arc4random_uniform(49)) + 1
+            setLottery.insert(temp)
+        }
+        var strLottery = ""
+        for lottery in setLottery {
+            strLottery.append("\(lottery) ")
+        }
+        let arrayLottery = Array(setLottery)
+        label1.text = String(arrayLottery[0])
+        label2.text = String(arrayLottery[1])
+        label3.text = String(arrayLottery[2])
+        label4.text = String(arrayLottery[3])
+        label5.text = String(arrayLottery[4])
+        label6.text = String(arrayLottery[5])
+        
+        return strLottery
+    }
 
 }
 
